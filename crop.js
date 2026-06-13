794b7f833b65a98a01ee08d0be3d4851c1c99995
Alexej Schneider <schneider.alex08112003@gmail.com>
2026-06-13 13:25:32 +0200

    style: aggressively crop favicon to maximize logo mark size

--- crop.js
+++ crop.js
@@ -1,11 +1,21 @@
 const Jimp = require("jimp");
+
 async function cropIcon() {
   const image = await Jimp.read("public/logo.png");
   const w = image.bitmap.width;
-  // Crop a square starting from the top-left (0,0) with size w x w
-  // This will grab the logo mark and cut off the text below it.
+  
+  // 1. Initial crop to get the top square
   image.crop(0, 0, w, w);
+  
+  // 2. The logo mark has internal whitespace padding.
+  // We want it as big as possible, so let's crop 15% off all 4 sides.
+  const cropAmount = Math.floor(w * 0.15);
+  const newW = w - (cropAmount * 2);
+  
+  image.crop(cropAmount, cropAmount, newW, newW);
+  
   await image.writeAsync("app/icon.png");
-  console.log("Cropped successfully to " + w + "x" + w);
+  console.log("Aggressive crop successfully to " + newW + "x" + newW);
 }
+
 cropIcon().catch(console.error);
